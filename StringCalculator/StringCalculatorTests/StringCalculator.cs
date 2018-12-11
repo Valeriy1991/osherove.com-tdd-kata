af6f5b79af2cb3b9f41ec7594100b724d27479a4
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-12-11 12:38:15 +0300

    Refactoring StringCalculator that all tests will be green

--- StringCalculator/StringCalculatorTests/StringCalculator.cs
+++ StringCalculator/StringCalculatorTests/StringCalculator.cs
@@ -1,10 +1,27 @@
-﻿namespace StringCalculatorTests
+﻿using System;
+
+namespace StringCalculatorTests
 {
     public class StringCalculator
     {
-        public int Add(string numbers)
+        public int Add(string numbersAsText)
         {
-            return 0;
+            if (string.Equals(numbersAsText, ""))
+                return 0;
+
+            var numbers = numbersAsText.Split(",");
+            if (numbers.Length == 1)
+                return Convert.ToInt32(numbers[0]);
+
+            if (numbers.Length == 2)
+            {
+                var firstNumber = Convert.ToInt32(numbers[0]);
+                var secondNumber = Convert.ToInt32(numbers[1]);
+
+                return firstNumber + secondNumber;
+            }
+
+            throw new InvalidOperationException();
         }
     }
 }

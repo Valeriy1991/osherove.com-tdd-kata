3e4c36e1adfce6cd33898eea113dd8b8228301e0
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-12-24 18:34:01 +0300

    Результаты упражения 1 скопированы в папку для упражнения 2

---
+++ StringCalculator/StringCalculatorTests/Kata2/StringCalculator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorTests.Kata2
+{
+    public class StringCalculator
+    {
+        public const string CustomSeparatorStartChar = "//";
+
+        public int Add(string inputNumbersAsText)
+        {
+            if (string.Equals(inputNumbersAsText, ""))
+                return 0;
+
+            List<int> numbers;
+
+            if (IsCustomSeparatorExists(inputNumbersAsText))
+            {
+                numbers = GetNumbersWithCustomSeparator(inputNumbersAsText).ToList();
+            }
+            else
+            {
+                numbers = GetNumbersWithDefaultSeparators(inputNumbersAsText).ToList();
+            }
+
+            RemoveNumbersThatBiggerThan1000(numbers);
+            CheckThatExistsNegativeNumbers(numbers);
+
+            return numbers.Sum();
+        }
+
+        private bool IsCustomSeparatorExists(string inputNumbersAsText)
+        {
+            var separatorSignStartPosition =
+                inputNumbersAsText.IndexOf(CustomSeparatorStartChar, StringComparison.Ordinal);
+            return separatorSignStartPosition == 0;
+        }
+
+        private IEnumerable<int> GetNumbersWithCustomSeparator(string inputNumbersAsText)
+        {
+            var customSeparatorRegex = new Regex(@"(?<=//)[^\n]+");
+            var separator = customSeparatorRegex.Match(inputNumbersAsText).Value;
+            var inputNumbersAsTextWithoutSeparator =
+                inputNumbersAsText.Substring(CustomSeparatorStartChar.Length + separator.Length);
+            
+            const string separatorGroupName = "separator";
+            var multipleCustomSeparatorsRegex = new Regex($@"(?<=\[)(?<{separatorGroupName}>[^\]]+)");
+            if (multipleCustomSeparatorsRegex.IsMatch(separator))
+            {
+                var foundSeparators = multipleCustomSeparatorsRegex.Matches(separator).Select(e => e.Value).ToList();
+                if (foundSeparators.Any())
+                {
+                    var firstCustomSeparator = foundSeparators[0];
+                    var otherCustomSeparators = foundSeparators.Skip(1);
+
+                    foreach (var otherCustomSeparator in otherCustomSeparators)
+                    {
+                        inputNumbersAsTextWithoutSeparator =
+                            inputNumbersAsTextWithoutSeparator.Replace(otherCustomSeparator, firstCustomSeparator);
+                    }
+
+                    separator = firstCustomSeparator;
+                }
+            }
+
+            var inputNumbersAsTextWithoutFirstNewLine = inputNumbersAsTextWithoutSeparator.Substring(1);
+            var numbersAsText = inputNumbersAsTextWithoutFirstNewLine.Split(separator);
+            return ConvertToNumbers(numbersAsText);
+        }
+
+        private IEnumerable<int> GetNumbersWithDefaultSeparators(string inputNumbersAsText)
+        {
+            var numbersAsTextWithOnlyCommaSeparator = inputNumbersAsText.Replace('\n', ',');
+            var numbersAsText = numbersAsTextWithOnlyCommaSeparator.Split(",");
+            return ConvertToNumbers(numbersAsText);
+        }
+
+        private IEnumerable<int> ConvertToNumbers(string[] numbersAsText)
+        {
+            return numbersAsText.Select(e => Convert.ToInt32(e));
+        }
+
+        private void RemoveNumbersThatBiggerThan1000(List<int> allNumbers)
+        {
+            allNumbers.RemoveAll(e => e > 1000);
+        }
+
+        private void CheckThatExistsNegativeNumbers(IEnumerable<int> allNumbers)
+        {
+            var allNegativeNumbers = allNumbers.Where(e => e < 0).ToList();
+            if (allNegativeNumbers.Any())
+            {
+                var messageBuilder = new StringBuilder();
+                messageBuilder.Append("negatives not allowed: ");
+                messageBuilder.Append(string.Join(", ", allNegativeNumbers));
+
+                throw new ArgumentException(messageBuilder.ToString());
+            }
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/Kata2/StringCalculatorTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace StringCalculatorTests.Kata2
+{
+    [ExcludeFromCodeCoverage]
+    [Trait("Category", "Unit")]
+    public class StringCalculatorTests
+    {
+        private StringCalculator CreateTestedComponent()
+        {
+            return new StringCalculator();
+        }
+
+        [Fact]
+        public void Add__InputStringIsEmpty__ReturnEmptyString()
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add("");
+            // Assert
+            Assert.Equal(0, sum);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("10")]
+        [InlineData("100")]
+        public void Add__InputStringContainsOneNumber__ReturnThisNumber(string inputNumberAsText)
+        {
+            // Arrange
+            var inputNumber = Convert.ToInt32(inputNumberAsText);
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumberAsText);
+            // Assert
+            Assert.Equal(inputNumber, sum);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("10,20", 30)]
+        [InlineData("100,200", 300)]
+        public void Add__InputStringContainsTwoNumbers_SeparatorIsComma__ReturnSumOfThisNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("1,2,3,4,5,6,7,8,9,10", 55)]
+        [InlineData("10,20,30,40,50,60", 210)]
+        [InlineData("100,200,300", 600)]
+        public void Add__InputStringContainsManyNumbers_SeparatorIsComma__ReturnSumOfThisNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+
+        [Theory]
+        [InlineData("1\n2\n3\n4\n5\n6\n7\n8\n9\n10", 55)]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("100\n200\n300", 600)]
+        public void Add__InputStringContainsManyNumbers_SeparatorIsNewLine__ReturnSumOfThisNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("//;\n5", 5)]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//-\n100-200-300", 600)]
+        public void Add__InputStringContainsDelimiterAtFirstLineAndAlsoContainsSomeNumbers__ReturnSumOfThisNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("//;\n-5", "-5")]
+        [InlineData("//;\n-1;-2", "-1, -2")]
+        [InlineData("//;\n-1;-2;3", "-1, -2")]
+        [InlineData("1\n2,-3", "-3")]
+        [InlineData("100,-200,300", "-200")]
+        [InlineData("10\n20\n30\n-40\n-50", "-40, -50")]
+        public void Add__InputStringContainsNegativeNumbers__ThrowExceptionWithThisNegativeNumbers(
+            string inputNumbersAsText, string validNegativeNumbersAsText)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            Action act = () => calculator.Add(inputNumbersAsText);
+            // Act
+            var ex = Record.Exception(act);
+            // Assert
+            Assert.IsType<ArgumentException>(ex);
+            Assert.Contains("negatives not allowed", ex.Message);
+            Assert.Contains(validNegativeNumbersAsText, ex.Message);
+        }
+
+        [Theory]
+        [InlineData("1,2,3,4,1001,1002", 10)]
+        [InlineData("1001,1002,1003", 0)]
+        [InlineData("1000,1001,1002,1003", 1000)]
+        public void
+            Add__InputStringContainsNumbersThatBiggerThan1000__ReturnSumOfThisNumbersWithoutNumbersThatBiggerThan1000(
+                string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("//***\n1***2***3***4", 10)]
+        [InlineData("//**\n10**20**30", 60)]
+        [InlineData("//++++\n999++++1000++++1001", 1999)]
+        public void Add__SeparatorsMayHaveAnyLength__ReturnSumOfInputNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("//[*][%]\n1*2*3%4%5", 15)]
+        [InlineData("//[#][^]\n10#20^30", 60)]
+        [InlineData("//[*][%]\n999*1000%1001", 1999)]
+        public void Add__InputStringContainsNumbersAndMultipleDelimiters__ReturnSumOfInputNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+
+        [Theory]
+        [InlineData("//[***][%%]\n1***2***3%%4%%5", 15)]
+        [InlineData("//[##][^]\n10##20^30", 60)]
+        [InlineData("//[*][%%%]\n999*1000%%%1001", 1999)]
+        public void Add__InputStringContainsNumbersAndMultipleDelimitersWithAnyLength__ReturnSumOfInputNumbers(
+            string inputNumbersAsText, int validSum)
+        {
+            // Arrange
+            var calculator = CreateTestedComponent();
+            // Act
+            var sum = calculator.Add(inputNumbersAsText);
+            // Assert
+            Assert.Equal(validSum, sum);
+        }
+    }
+}
